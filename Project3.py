c73c286ba9f5107a47ea61b096c4fc5df6cd8420
Vivian Feng <112654276+vivfeng@users.noreply.github.com>
2023-03-15 14:47:17 -0700

    Update Project3.py

--- Project3.py
+++ Project3.py
@@ -1,15 +1,78 @@
-#(A) Choice of Population, with supporting expenditure
-
-print('We chose to anlayze the Ugandan popultion of males and females 19-30') 
-
-#(A) Estimate Demand System
-
-#(A) Presentation
-
-#(A) Code Review
-
-#(B) Nutritional Content of Different Foods
-
-#(B) Nutritional Adequacy of Diet
-
-#(C) Counterfactual Experiments
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "97268c66-0e11-476d-ad47-1bfda37eec2c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#(A) Choice of Population, with supporting expenditure\n",
+    "\n",
+    "print('We chose to anlayze the Ugandan popultion of males and females 19-30') \n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d9983f62-c998-42d0-9178-d7ba1b921108",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#(A) Estimate Demand System"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e7013bab-91b6-4ba4-9310-8c7ad4883cfa",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#(B) Nutritional Content of Different Foods"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a3a8f478-dec7-44a3-aa4a-f13636a3b5fb",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#(B) Nutritional Adequacy of Diet"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4661ddab-f910-4d38-87f4-caa68c07da64",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#(C) Counterfactual Experiments"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "anaconda-2022.05-py39",
+   "language": "python",
+   "name": "conda-env-anaconda-2022.05-py39-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
