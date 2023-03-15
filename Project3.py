0cc78868e2ff6f25acdaab5afd264511a6e6f8cb
Vivian Feng <112654276+vivfeng@users.noreply.github.com>
2023-03-15 14:45:08 -0700

    Add files via upload

---
+++ Project3.py
@@ -0,0 +1,15 @@
+#(A) Choice of Population, with supporting expenditure
+
+print('We chose to anlayze the Ugandan popultion of males and females 19-30') 
+
+#(A) Estimate Demand System
+
+#(A) Presentation
+
+#(A) Code Review
+
+#(B) Nutritional Content of Different Foods
+
+#(B) Nutritional Adequacy of Diet
+
+#(C) Counterfactual Experiments
